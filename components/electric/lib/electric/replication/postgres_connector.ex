18448df5389f338e80dee5218bb17e4a947542a0
Oleksii Sholik <oleksii@sholik.dev>
2024-02-27 13:29:51 +0200

    Expose WAL cache and replication stream pos in PostgresConnector API
    The PostgresConnector serves as an entry point to starting the
    supervision tree of processes that are connected to Postgres or
    carry out related tasks.
    
    This change is an attempt to start shaping up a "facade API" in the
    PostgresConnector module to abstract away internal details of the
    modules it uses in its implementation.

--- components/electric/lib/electric/replication/postgres_connector.ex
+++ components/electric/lib/electric/replication/postgres_connector.ex
@@ -3,6 +3,7 @@ defmodule Electric.Replication.PostgresConnector do
 
   require Logger
 
+  alias Electric.Postgres.Lsn
   alias Electric.Replication.Connectors
   alias Electric.Replication.PostgresConnectorMng
   alias Electric.Replication.PostgresConnectorSup
@@ -71,6 +72,39 @@ defmodule Electric.Replication.PostgresConnector do
   def connector_config(origin) do
     PostgresConnectorMng.connector_config(origin)
   end
+
+  ###
+  # WAL window
+  ###
+
+  def client_wal_window_status(origin, client_pos) do
+    client_lsn = Lsn.from_integer(client_pos)
+
+    cond do
+      Lsn.compare(client_lsn, current_lsn(origin)) == :gt -> :behind_window
+      client_pos_in_cached_window?(origin, client_pos) -> :cached
+      lsn_in_resumable_window?(origin, client_lsn) -> :resumable
+      true -> :behind_window
+    end
+  end
+
+  def client_pos_in_cached_window?(_origin, lsn) do
+    Electric.Postgres.CachedWal.Api.lsn_in_cached_window?(lsn)
+  end
+
+  def lsn_in_resumable_window?(origin, lsn) do
+    Lsn.compare(lsn, main_slot_lsn(origin)) != :lt
+  end
+
+  defp main_slot_lsn(origin) do
+    Electric.Replication.Postgres.LogicalReplicationProducer.main_slot_lsn(origin)
+  end
+
+  defp current_lsn(origin) do
+    Electric.Replication.Postgres.LogicalReplicationProducer.current_lsn(origin)
+  end
+
+  ###
 
   defp log_connector_sup_startup_error({:ok, _sup_pid} = ok, _connector_config), do: ok
 
